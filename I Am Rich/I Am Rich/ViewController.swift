17c5822bf9b798069aa3a6a681a4f17547cf5caa
bryanvdang <bryandang@Bryans-MacBook-Pro.local>
2020-04-10 00:00:12 -0700

    I am Rich App creation

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Bryan Dang on 4/9/20.
+//  Copyright © 2020 Bryan Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
