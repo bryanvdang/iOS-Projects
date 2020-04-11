1b667407591e1ae4a715ecad1c359f07b8891313
bryanvdang <bryandang@Bryans-MacBook-Pro.local>
2020-04-11 12:59:09 -0700

    Created/finished I am Poor App

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Bryan Dang on 4/11/20.
+//  Copyright © 2020 Bryan Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
