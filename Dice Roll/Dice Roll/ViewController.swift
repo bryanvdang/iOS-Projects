85c0d92e119adc9dacfc42ba3a77acc591b32d0e
bryanvdang <bryandang@Bryans-MacBook-Pro.local>
2020-04-13 14:02:46 -0700

    created/finished Dice rolling app

---
+++ Dice Roll/Dice Roll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice Roll
+//
+//  Created by Bryan Dang on 4/13/20.
+//  Copyright © 2020 Bryan Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
